6886a892ef377ba87480bf2898badcf19fa7b0dd
Siva <hariv67@gmail.com>
2021-08-09 16:42:30 +0530

    first commit

---
+++ src/main/java/coverity/Resource_leak.java
@@ -0,0 +1,41 @@
+package coverity;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.mysql.cj.protocol.Resultset;
+
+public class Resource_leak {
+	
+	  static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/?user=student";
+	   static final String USER = "student";
+	   static final String PASS = "student";
+	   static final String QUERY = "SELECT top 10 * FROM marksheet";
+	   
+
+	
+	public static void main(String args[]) {
+		   try{
+		
+			   Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			   Statement stmt = conn.createStatement();
+		       ResultSet rs = stmt.executeQuery(QUERY);
+		       String inputFile = "siva";
+			   BufferedReader br = new BufferedReader(new FileReader(inputFile ));
+			   while(rs.next()) {
+				   System.out.println("name:" + rs.getString("Name"));
+			   }
+			   //connection prep stmt result set not closed
+			   
+		   }
+		   catch(Exception e) {
+			   System.out.println("broken connection..");
+		   }
+	}
+
+}
