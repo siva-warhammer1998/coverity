26c600c43b1ac10452318b6d2c50705cae90663e
Siva <hariv67@gmail.com>
2021-08-10 11:13:47 +0530

    resource_leak check

--- src/main/java/coverity/Resource_leak.java
+++ src/main/java/coverity/Resource_leak.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.mysql.cj.protocol.Resultset;
@@ -18,7 +19,18 @@ public class Resource_leak {
 	   static final String QUERY = "SELECT top 10 * FROM marksheet";
 	   
 
-	
+	   public static void check_leakage(Connection conn) {
+			String QUERY1 = "SELECT * FROM marksheet";
+			Statement stmt;
+			try {
+				stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(QUERY1);
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+		
+		}
 	public static void main(String args[]) {
 		   try{
 		
@@ -27,15 +39,22 @@ public class Resource_leak {
 		       ResultSet rs = stmt.executeQuery(QUERY);
 		       String inputFile = "siva";
 			   BufferedReader br = new BufferedReader(new FileReader(inputFile ));
+			   check_leakage(conn);
 			   while(rs.next()) {
 				   System.out.println("name:" + rs.getString("Name"));
 			   }
+			   
 			   //connection prep stmt result set not closed
+			   conn.close();
+			   rs.close();
+			   stmt.close();
+			   
 			   
 		   }
 		   catch(Exception e) {
 			   System.out.println("broken connection..");
 		   }
 	}
-
+	
+	
 }
